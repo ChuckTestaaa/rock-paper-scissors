6df93f0841d51218e1d760cdb5f8d01d2967ccfe
Chuck Testa <112298806+ChuckTestaaa@users.noreply.github.com>
2025-08-02 12:16:42 +0800

    Initialize 2 functions for computer choice
    Create two functions,  getComputerChoice  returns a random choice and getRandomInt.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,19 @@
 console.log('Hello World')
+
+function getComputerChoice() {
+  let max = 3
+  let randomNumber = getRandomInt(max)
+
+  if (randomNumber === 0) {
+    return 'rock'
+  } else if (randomNumber === 1) {
+    return 'paper'
+  }
+
+  return 'scissors'
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
